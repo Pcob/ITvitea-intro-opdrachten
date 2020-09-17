3484a4208080b59c7173c06d1b329ed5a346d913
Pcob <71030608+Pcob@users.noreply.github.com>
2020-09-17 21:00:51 +0200

    Opdracht 2: Vlaggen van de wereld
    Dit is de opdracht van het maken van de vlaggen van de wereld. Dit heb ik echter ook weer met behulp van de volgende YouTube video gedaan: 
    
    https://www.youtube.com/watch?v=hQDjz2ISklw&ab_channel=MooICT

--- Form1.cs
+++ Form1.cs
@@ -8,75 +8,245 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace ITvitea_opdrachten_juist
+namespace ITvitea_opdrachten_vlaggen_juiste
 {
-    public partial class Calculator : Form
+    public partial class Form1 : Form
     {
-        Double value = 0;
-        String operation = "";
-        bool operation_pressed = false;
-        public Calculator()
+
+        int correctAnswer;
+        int questionNumber = 1;
+        int score;
+        int percentage;
+        int totalQuestions;
+
+
+        public Form1()
         {
             InitializeComponent();
+
+            askQuestion(questionNumber);
+
+            totalQuestions = 13;
         }
 
-
-
-        private void button_Click(object sender, EventArgs e)
+        private void checkAnswerEvent(object sender, EventArgs e)
         {
-            if ((result.Text == "0")||(operation_pressed))
-                result.Clear();
+            var senderObject = (Button)sender;
 
-            operation_pressed = false;
-            Button b = (Button)sender; 
-            result.Text = result.Text + b.Text; 
-        }
+            int buttonTag = Convert.ToInt32(senderObject.Tag);
 
-        private void button17_Click(object sender, EventArgs e)
-        {
-            result.Text = "0"; 
-        }
-
-        private void operator_click(object sender, EventArgs e)
-        {
-            Button b = (Button)sender;
-            operation = b.Text;
-            value = Double.Parse(result.Text);
-            operation_pressed = true;
-            equation.Text = value + " " + operation;
-
-        }
-
-        private void button16_Click(object sender, EventArgs e)
-        {
-            
-            equation.Text = " ";
-            switch (operation)
+            if(buttonTag == correctAnswer)
             {
-                case "+":
-                    result.Text = (value + Double.Parse(result.Text)).ToString();
-                    break;
-                case "-":
-                    result.Text = (value - Double.Parse(result.Text)).ToString();
-                    break;
-                case "*":
-                    result.Text = (value * Double.Parse(result.Text)).ToString();
-                    break;
-                case "/":
-                    result.Text = (value / Double.Parse(result.Text)).ToString();
-                    break;
-                default:
-                    break;
+                score++;
+            }
+
+            if(questionNumber == totalQuestions)
+            {
+                percentage = (int)Math.Round((double)(score * 100) / totalQuestions);
+
+                MessageBox.Show(
+                    "Einde van Quiz" + Environment.NewLine + 
+                    "Je hebt : " + score + "goed" + Environment.NewLine + 
+                    "Jouw percentage is " + percentage + "%" + Environment.NewLine +
+                    "Klik op OK opnieuw te beginnen"
+                    );
+                score = 0;
+                questionNumber = 0;
+                askQuestion(questionNumber);
 
             }
-            
+
+            questionNumber++;
+            askQuestion(questionNumber);
         }
 
-        private void button18_Click(object sender, EventArgs e)
+        private void askQuestion(int qnum)
         {
-            result.Text = "0";
-            value = 0;
+            switch(qnum)
+            {
+                case 1:
+                    pictureBox1.Image = Properties.Resources.Rusland_vlag;
 
+                    lblQuestion.Text = "Uit welk land komt deze vlag?";
+
+                    button1.Text = "Oostenrijk";
+                    button2.Text = "Rusland";
+                    button3.Text = "Duitsland";
+                    button4.Text = "Italie";
+
+                    correctAnswer = 2;
+
+
+                    break;
+                case 2:
+                    pictureBox1.Image = Properties.Resources.VlagBurgarije;
+
+                    lblQuestion.Text = "Uit welk land komt deze vlag?";
+
+                    button1.Text = "Bulgarije";
+                    button2.Text = "Italie";
+                    button3.Text = "Zwitserland";
+                    button4.Text = "Frankrijk";
+
+                    correctAnswer = 1;
+
+
+                    break;
+                case 3:
+                    pictureBox1.Image = Properties.Resources.Cyprus;
+
+                    lblQuestion.Text = "Uit welk land komt deze vlag?";
+
+                    button1.Text = "Griekenland";
+                    button2.Text = "Turkije";
+                    button3.Text = "Cyprus";
+                    button4.Text = "Macedonie";
+
+                    correctAnswer = 3;
+
+
+                    break;
+                case 4:
+                    pictureBox1.Image = Properties.Resources.Denmark;
+
+                    lblQuestion.Text = "Uit welk land komt deze vlag?";
+
+                    button1.Text = "Belgie";
+                    button2.Text = "Zweden";
+                    button3.Text = "Noorwegen";
+                    button4.Text = "Denenmarken";
+
+                    correctAnswer = 4;
+
+
+                    break;
+                case 5:
+                    pictureBox1.Image = Properties.Resources.Ivoorkust;
+
+                    lblQuestion.Text = "Uit welk land komt deze vlag?";
+
+                    button1.Text = "Marokko";
+                    button2.Text = "Ivoorkust";
+                    button3.Text = "Ghana";
+                    button4.Text = "Egypte";
+
+                    correctAnswer = 2;
+
+
+                    break;
+                case 6:
+                    pictureBox1.Image = Properties.Resources.Afrika;
+
+                    lblQuestion.Text = "Uit welk land komt deze vlag?";
+
+                    button1.Text = "Namibie";
+                    button2.Text = "Kenia";
+                    button3.Text = "Zuid-Afrika";
+                    button4.Text = "Botszwana";
+
+                    correctAnswer = 3;
+
+
+                    break;
+                case 7:
+                    pictureBox1.Image = Properties.Resources.Canada;
+
+                    lblQuestion.Text = "Uit welk land komt deze vlag?";
+
+                    button1.Text = "Canada";
+                    button2.Text = "Verenigde Staten";
+                    button3.Text = "Mexico";
+                    button4.Text = "Florida";
+
+                    correctAnswer = 1;
+
+
+                    break;
+                case 8:
+                    pictureBox1.Image = Properties.Resources.Groenland;
+
+                    lblQuestion.Text = "Uit welk land komt deze vlag?";
+
+                    button1.Text = "Verenidge Staten";
+                    button2.Text = "Alaska";
+                    button3.Text = "Groenland";
+                    button4.Text = "IJsland";
+
+                    correctAnswer = 3;
+
+
+                    break;
+                case 9:
+                    pictureBox1.Image = Properties.Resources.Venuzuale;
+
+                    lblQuestion.Text = "Uit welk land komt deze vlag?";
+
+                    button1.Text = "Argentinie";
+                    button2.Text = "Venuzuala";
+                    button3.Text = "Brazilie";
+                    button4.Text = "Colombia";
+
+                    correctAnswer = 2;
+
+
+                    break;
+                case 10:
+                    pictureBox1.Image = Properties.Resources.Uruguay;
+
+                    lblQuestion.Text = "Uit welk land komt deze vlag?";
+
+                    button1.Text = "Chili";
+                    button2.Text = "Suriname";
+                    button3.Text = "Bolivia";
+                    button4.Text = "Uruguay";
+
+                    correctAnswer = 4;
+
+
+                    break;
+                case 11:
+                    pictureBox1.Image = Properties.Resources.Zuid_Korea;
+
+                    lblQuestion.Text = "Uit welk land komt deze vlag?";
+
+                    button1.Text = "China";
+                    button2.Text = "Taiwan";
+                    button3.Text = "Zuid-Korea";
+                    button4.Text = "Japan";
+
+                    correctAnswer = 3;
+
+
+                    break;
+                case 12:
+                    pictureBox1.Image = Properties.Resources.India;
+
+                    lblQuestion.Text = "Uit welk land komt deze vlag?";
+
+                    button1.Text = "India";
+                    button2.Text = "Nepal";
+                    button3.Text = "Tibet";
+                    button4.Text = "Pakistan";
+
+                    correctAnswer = 1;
+
+
+                    break;
+                case 13:
+                    pictureBox1.Image = Properties.Resources.New_Zeeland;
+
+                    lblQuestion.Text = "Uit welk land komt deze vlag?";
+
+                    button1.Text = "Australie";
+                    button2.Text = "Nieuw-Zeeland";
+                    button3.Text = "Tahiti";
+                    button4.Text = "Indonesie";
+
+                    correctAnswer = 2;
+
+
+                    break;
+            }
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,70 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
-namespace Intro_to_C_Development
+namespace ITvitea_opdrachten_vlaggen_juiste
 {
-    class Program
+    static class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
         {
-            // Console.WriteLine("Please your first name: ");
-            // string first_name = Console.ReadLine();
-           
-
-            // Console.WriteLine("Please your Last name: ");
-            // string Last_Name = Console.ReadLine();
-            // Console.WriteLine("Hoi " + first_name + Last_Name);
-
-            // Console.WriteLine("Piet's big Giveaway");
-            // Console.Write("Choose a door: 1, 2 or 3: ");
-            // string userValue = Console.ReadLine();
-
-            // if (userValue == "1")
-            // {
-            //     string message = "You won a new car!";
-            //     Console.WriteLine(message);
-
-            // }
-            // if (userValue == "2")
-            // {
-            //     string message = "You won a bal!";
-            //     Console.WriteLine(message);
-            // }
-            // else
-            // {
-            //     string message = "Error melding";
-            //     Console.WriteLine(message);
-            // }
-            // Console.ReadLine();
-
-            // for (int i = 0; i < 12; i++)
-            // {
-            // Console.WriteLine(i);
-            // } 
-
-            // Console.WriteLine("Your credentials: ");
-            // Console.Write("Type your first name: ");
-            // String first_name = Console.ReadLine();
-
-            // Console.Write("Type your last name: ");
-            // String last_name = Console.ReadLine();
-            // Console.WriteLine("Hello, " + first_name + " " + last_name + " You are very welcome");
-
-            // Console.WriteLine("What is the main City of Norway? ");
-            // String maincity = Console.ReadLine();
-
-            // if (maincity == "Oslo")
-            // {
-            //       Console.WriteLine("The answer is true");
-            // }
-            // else
-            // {
-            //     Console.WriteLine("False answer");
-            // }
-
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
         }
     }
 }
