321f86cc1a654ecddecc54f14588593a38bfd69e
Pcob <71030608+Pcob@users.noreply.github.com>
2020-10-01 18:46:18 +0200

    Opdracht 3: Versleutelen
    Deze opdracht heb ik gemaakt met behulp van de volgende YouTube video: https://www.youtube.com/watch?v=CFfjZfkciQQ&ab_channel=AnMaiH%C3%A0

---
+++ Versleutelen.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Encryptie
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbPath = new System.Windows.Forms.TextBox();
+            this.btBrowse = new System.Windows.Forms.Button();
+            this.rbEncrypt = new System.Windows.Forms.RadioButton();
+            this.rbDecrypt = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.btStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "File path";
+            // 
+            // tbPath
+            // 
+            this.tbPath.Location = new System.Drawing.Point(16, 34);
+            this.tbPath.Name = "tbPath";
+            this.tbPath.ReadOnly = true;
+            this.tbPath.Size = new System.Drawing.Size(687, 22);
+            this.tbPath.TabIndex = 1;
+            // 
+            // btBrowse
+            // 
+            this.btBrowse.Location = new System.Drawing.Point(710, 32);
+            this.btBrowse.Name = "btBrowse";
+            this.btBrowse.Size = new System.Drawing.Size(75, 24);
+            this.btBrowse.TabIndex = 2;
+            this.btBrowse.Text = "Browse";
+            this.btBrowse.UseVisualStyleBackColor = true;
+            this.btBrowse.Click += new System.EventHandler(this.btBrowse_Click);
+            // 
+            // rbEncrypt
+            // 
+            this.rbEncrypt.AutoSize = true;
+            this.rbEncrypt.Location = new System.Drawing.Point(16, 72);
+            this.rbEncrypt.Name = "rbEncrypt";
+            this.rbEncrypt.Size = new System.Drawing.Size(77, 21);
+            this.rbEncrypt.TabIndex = 3;
+            this.rbEncrypt.TabStop = true;
+            this.rbEncrypt.Text = "Encrypt";
+            this.rbEncrypt.UseVisualStyleBackColor = true;
+            this.rbEncrypt.CheckedChanged += new System.EventHandler(this.rbEncrypt_CheckedChanged);
+            // 
+            // rbDecrypt
+            // 
+            this.rbDecrypt.AutoSize = true;
+            this.rbDecrypt.Location = new System.Drawing.Point(99, 72);
+            this.rbDecrypt.Name = "rbDecrypt";
+            this.rbDecrypt.Size = new System.Drawing.Size(78, 21);
+            this.rbDecrypt.TabIndex = 4;
+            this.rbDecrypt.TabStop = true;
+            this.rbDecrypt.Text = "Decrypt";
+            this.rbDecrypt.UseVisualStyleBackColor = true;
+            this.rbDecrypt.CheckedChanged += new System.EventHandler(this.rbDecrypt_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Password";
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Location = new System.Drawing.Point(112, 93);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.PasswordChar = '*';
+            this.tbPassword.Size = new System.Drawing.Size(253, 22);
+            this.tbPassword.TabIndex = 6;
+            // 
+            // btStart
+            // 
+            this.btStart.Location = new System.Drawing.Point(16, 138);
+            this.btStart.Name = "btStart";
+            this.btStart.Size = new System.Drawing.Size(687, 57);
+            this.btStart.TabIndex = 7;
+            this.btStart.Text = "Start";
+            this.btStart.UseVisualStyleBackColor = true;
+            this.btStart.Click += new System.EventHandler(this.btStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 224);
+            this.Controls.Add(this.btStart);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.rbDecrypt);
+            this.Controls.Add(this.rbEncrypt);
+            this.Controls.Add(this.btBrowse);
+            this.Controls.Add(this.tbPath);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Bestand Encryptie";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbPath;
+        private System.Windows.Forms.Button btBrowse;
+        private System.Windows.Forms.RadioButton rbEncrypt;
+        private System.Windows.Forms.RadioButton rbDecrypt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.Button btStart;
+    }
+}
+
