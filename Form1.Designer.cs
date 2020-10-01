4d25d54ae52f9c831aeecfff947dd9f7e5fc9bd9
Pcob <71030608+Pcob@users.noreply.github.com>
2020-10-01 18:37:17 +0200

    Opdracht 6: Lettertype Chaos
    Deze opdracht heb ik gemaakt met behulp van het volgende YouTube video: https://www.youtube.com/watch?v=Okv0Bpeqg3U&ab_channel=codefactory2016

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Encryptie
+﻿namespace Lettertype_chaos
 {
     partial class Form1
     {
@@ -28,125 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.tbPath = new System.Windows.Forms.TextBox();
-            this.btBrowse = new System.Windows.Forms.Button();
-            this.rbEncrypt = new System.Windows.Forms.RadioButton();
-            this.rbDecrypt = new System.Windows.Forms.RadioButton();
-            this.label2 = new System.Windows.Forms.Label();
-            this.tbPassword = new System.Windows.Forms.TextBox();
-            this.btStart = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // label1
+            // richTextBox1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 13);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(62, 17);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "File path";
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(505, 426);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
             // 
-            // tbPath
+            // button1
             // 
-            this.tbPath.Location = new System.Drawing.Point(16, 34);
-            this.tbPath.Name = "tbPath";
-            this.tbPath.ReadOnly = true;
-            this.tbPath.Size = new System.Drawing.Size(687, 22);
-            this.tbPath.TabIndex = 1;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(543, 34);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(201, 40);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Lettertype en grootte";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // btBrowse
+            // button2
             // 
-            this.btBrowse.Location = new System.Drawing.Point(710, 32);
-            this.btBrowse.Name = "btBrowse";
-            this.btBrowse.Size = new System.Drawing.Size(75, 24);
-            this.btBrowse.TabIndex = 2;
-            this.btBrowse.Text = "Browse";
-            this.btBrowse.UseVisualStyleBackColor = true;
-            this.btBrowse.Click += new System.EventHandler(this.btBrowse_Click);
-            // 
-            // rbEncrypt
-            // 
-            this.rbEncrypt.AutoSize = true;
-            this.rbEncrypt.Location = new System.Drawing.Point(16, 72);
-            this.rbEncrypt.Name = "rbEncrypt";
-            this.rbEncrypt.Size = new System.Drawing.Size(77, 21);
-            this.rbEncrypt.TabIndex = 3;
-            this.rbEncrypt.TabStop = true;
-            this.rbEncrypt.Text = "Encrypt";
-            this.rbEncrypt.UseVisualStyleBackColor = true;
-            this.rbEncrypt.CheckedChanged += new System.EventHandler(this.rbEncrypt_CheckedChanged);
-            // 
-            // rbDecrypt
-            // 
-            this.rbDecrypt.AutoSize = true;
-            this.rbDecrypt.Location = new System.Drawing.Point(99, 72);
-            this.rbDecrypt.Name = "rbDecrypt";
-            this.rbDecrypt.Size = new System.Drawing.Size(78, 21);
-            this.rbDecrypt.TabIndex = 4;
-            this.rbDecrypt.TabStop = true;
-            this.rbDecrypt.Text = "Decrypt";
-            this.rbDecrypt.UseVisualStyleBackColor = true;
-            this.rbDecrypt.CheckedChanged += new System.EventHandler(this.rbDecrypt_CheckedChanged);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(13, 96);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(69, 17);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "Password";
-            // 
-            // tbPassword
-            // 
-            this.tbPassword.Location = new System.Drawing.Point(112, 93);
-            this.tbPassword.Name = "tbPassword";
-            this.tbPassword.PasswordChar = '*';
-            this.tbPassword.Size = new System.Drawing.Size(253, 22);
-            this.tbPassword.TabIndex = 6;
-            // 
-            // btStart
-            // 
-            this.btStart.Location = new System.Drawing.Point(16, 138);
-            this.btStart.Name = "btStart";
-            this.btStart.Size = new System.Drawing.Size(687, 57);
-            this.btStart.TabIndex = 7;
-            this.btStart.Text = "Start";
-            this.btStart.UseVisualStyleBackColor = true;
-            this.btStart.Click += new System.EventHandler(this.btStart_Click);
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(543, 93);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(201, 40);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Lettertype kleur";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 224);
-            this.Controls.Add(this.btStart);
-            this.Controls.Add(this.tbPassword);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.rbDecrypt);
-            this.Controls.Add(this.rbEncrypt);
-            this.Controls.Add(this.btBrowse);
-            this.Controls.Add(this.tbPath);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
             this.Name = "Form1";
-            this.Text = "Bestand Encryptie";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Text = "Form1";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox tbPath;
-        private System.Windows.Forms.Button btBrowse;
-        private System.Windows.Forms.RadioButton rbEncrypt;
-        private System.Windows.Forms.RadioButton rbDecrypt;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox tbPassword;
-        private System.Windows.Forms.Button btStart;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,140 +7,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.IO;
 
-namespace Encryptie
+namespace Lettertype_chaos
 {
     public partial class Form1 : Form
     {
-        byte[] abc;
-        byte[,] table;
-
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void btBrowse_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
-            OpenFileDialog od = new OpenFileDialog();
-            od.Multiselect = false;
-            if (od.ShowDialog() == DialogResult.OK) ;
+            FontDialog fd = new FontDialog();
+            if(fd.ShowDialog()== DialogResult.OK)
             {
-                tbPath.Text = od.FileName; 
+                richTextBox1.Font = fd.Font;
             }
         }
 
-        private void rbEncrypt_CheckedChanged(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e)
         {
-            if(rbEncrypt.Checked)
-            {
-                rbDecrypt.Checked = false;
-            }
-        }
-
-        private void rbDecrypt_CheckedChanged(object sender, EventArgs e)
-        {
-            if (rbDecrypt.Checked)
-            {
-                rbEncrypt.Checked = false;
-            }
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            rbEncrypt.Checked = true;
-            abc = new byte[256];
-            for (int i = 0; i < 256; i++)
-                abc[i] = Convert.ToByte(i);
-            table = new byte[256, 256];
-            for (int i = 0; i < 256; i++)
-                for (int j = 0; j < 256; j++)
-                {
-                    table[i, j] = abc[(i + j) % 256];
-
-                }
-        }
-
-        private void btStart_Click(object sender, EventArgs e)
-        {
-            if (!File.Exists(tbPath.Text))
-            {
-                MessageBox.Show("Het bestand bestaat niet.");
-                return;
-            }
-            if(String.IsNullOrEmpty(tbPassword.Text))
-            {
-                MessageBox.Show("Leeg wachtwoord. Type je wachtwoord");
-                return;
-            }
-            try
-            {
-                byte[] fileContent = File.ReadAllBytes(tbPath.Text);
-                byte[] passwordTmp = Encoding.ASCII.GetBytes(tbPassword.Text);
-                byte[] keys = new byte[fileContent.Length];
-                for (int i = 0; i < fileContent.Length; i++)
-                    keys[i] = passwordTmp[i % passwordTmp.Length];
-
-                byte[] result = new byte[fileContent.Length];
-
-                if (rbEncrypt.Checked)
-                {
-                    for(int i = 0; i < fileContent.Length; i++)
-                    {
-                        byte value = fileContent[i];
-                        byte key = keys[i];
-                        int valueIndex = -1, KeyIndex = -1;
-                        for (int j = 0; j < 256; j++)
-                            if(abc[j] == value)
-                            {
-                                valueIndex = j;
-                                break;
-                            }
-                        for(int j = 0; j < 256; j++)
-                            if(abc[j] == key)
-                            {
-                                KeyIndex = j;
-                                break;
-                            }
-                        result[i] = table[KeyIndex, valueIndex];
-                    }
-                }
-                else
-                {
-                    for (int i = 0; i < fileContent.Length; i++)
-                    {
-                        byte value = fileContent[i];
-                        byte key = keys[i];
-                        int valueIndex = -1, KeyIndex = -1;
-                        for (int j = 0; j < 256; j++)
-                            if (abc[j] == key)
-                            {
-                                KeyIndex = j;
-                                break;
-                            }
-                        for (int j = 0; j < 256; j++)
-                            if (table[KeyIndex, j] == value)
-                            {
-                                valueIndex = j;
-                                break;
-                            }
-                        result[i] = abc[valueIndex];
-                    }
-                }
-                String fileExt = Path.GetExtension(tbPath.Text);
-                SaveFileDialog sd = new SaveFileDialog();
-                sd.Filter = "Files (" + fileExt + ") | *" + fileExt;
-                if(sd.ShowDialog() == DialogResult.OK)
-                {
-                    File.WriteAllBytes(sd.FileName, result);
-                }
-            }
-            catch
-            {
-                MessageBox.Show("Bestand is in gebruik. Kies een ander bestand of sluit het bestand dat je wilt open");
-                return;
-            }
+            ColorDialog col = new ColorDialog();
+            col.ShowDialog();
+            richTextBox1.SelectionColor = col.Color;
         }
     }
 }
