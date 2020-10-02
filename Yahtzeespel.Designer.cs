dc61c0f48a3836c74fe505f5fea3dae135a15556
Pcob <71030608+Pcob@users.noreply.github.com>
2020-10-03 00:11:15 +0200

    Opdracht 8: Yahtzeespel
    Deze opdracht heb ik met behulp van de volgende drie YouTube video's gemaakt: 
    
    https://www.youtube.com/watch?v=vic52OZM6Ho&ab_channel=IndieDevelopment
    https://www.youtube.com/watch?v=IhmHiSuvGfs&ab_channel=IndieDevelopment
    https://www.youtube.com/watch?v=MqNDYIoLsi0&ab_channel=IndieDevelopment

---
+++ Yahtzeespel.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace Yahtzeespel2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.P1dobbel_1 = new System.Windows.Forms.Label();
+            this.P1dobbel_4 = new System.Windows.Forms.Label();
+            this.P1dobbel_3 = new System.Windows.Forms.Label();
+            this.P1dobbel_2 = new System.Windows.Forms.Label();
+            this.P1dobbel_5 = new System.Windows.Forms.Label();
+            this.Rollen1 = new System.Windows.Forms.Button();
+            this.lblP1_displayResults = new System.Windows.Forms.Label();
+            this.lblP2_displayResults = new System.Windows.Forms.Label();
+            this.Rollen2 = new System.Windows.Forms.Button();
+            this.P2dobbel_5 = new System.Windows.Forms.Label();
+            this.P2dobbel_2 = new System.Windows.Forms.Label();
+            this.P2dobbel_3 = new System.Windows.Forms.Label();
+            this.P2dobbel_4 = new System.Windows.Forms.Label();
+            this.P2dobbel_1 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lbl_p1Name = new System.Windows.Forms.Label();
+            this.lbl_p2Name = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // P1dobbel_1
+            // 
+            this.P1dobbel_1.Image = global::Yahtzeespel2.Properties.Resources.blank;
+            this.P1dobbel_1.Location = new System.Drawing.Point(106, 113);
+            this.P1dobbel_1.Name = "P1dobbel_1";
+            this.P1dobbel_1.Size = new System.Drawing.Size(157, 157);
+            this.P1dobbel_1.TabIndex = 0;
+            // 
+            // P1dobbel_4
+            // 
+            this.P1dobbel_4.Image = global::Yahtzeespel2.Properties.Resources.blank;
+            this.P1dobbel_4.Location = new System.Drawing.Point(207, 288);
+            this.P1dobbel_4.Name = "P1dobbel_4";
+            this.P1dobbel_4.Size = new System.Drawing.Size(156, 154);
+            this.P1dobbel_4.TabIndex = 1;
+            // 
+            // P1dobbel_3
+            // 
+            this.P1dobbel_3.Image = global::Yahtzeespel2.Properties.Resources.blank;
+            this.P1dobbel_3.Location = new System.Drawing.Point(485, 118);
+            this.P1dobbel_3.Name = "P1dobbel_3";
+            this.P1dobbel_3.Size = new System.Drawing.Size(155, 152);
+            this.P1dobbel_3.TabIndex = 2;
+            // 
+            // P1dobbel_2
+            // 
+            this.P1dobbel_2.Image = global::Yahtzeespel2.Properties.Resources.blank;
+            this.P1dobbel_2.Location = new System.Drawing.Point(297, 112);
+            this.P1dobbel_2.Name = "P1dobbel_2";
+            this.P1dobbel_2.Size = new System.Drawing.Size(159, 158);
+            this.P1dobbel_2.TabIndex = 3;
+            // 
+            // P1dobbel_5
+            // 
+            this.P1dobbel_5.Image = global::Yahtzeespel2.Properties.Resources.blank;
+            this.P1dobbel_5.Location = new System.Drawing.Point(416, 288);
+            this.P1dobbel_5.Name = "P1dobbel_5";
+            this.P1dobbel_5.Size = new System.Drawing.Size(164, 156);
+            this.P1dobbel_5.TabIndex = 4;
+            // 
+            // Rollen1
+            // 
+            this.Rollen1.Location = new System.Drawing.Point(300, 509);
+            this.Rollen1.Name = "Rollen1";
+            this.Rollen1.Size = new System.Drawing.Size(200, 50);
+            this.Rollen1.TabIndex = 5;
+            this.Rollen1.Text = "Gooi de dobbelstenen";
+            this.Rollen1.UseVisualStyleBackColor = true;
+            this.Rollen1.Click += new System.EventHandler(this.Rollen1_Click);
+            // 
+            // lblP1_displayResults
+            // 
+            this.lblP1_displayResults.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.lblP1_displayResults.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblP1_displayResults.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblP1_displayResults.Location = new System.Drawing.Point(258, 571);
+            this.lblP1_displayResults.Name = "lblP1_displayResults";
+            this.lblP1_displayResults.Size = new System.Drawing.Size(304, 64);
+            this.lblP1_displayResults.TabIndex = 6;
+            this.lblP1_displayResults.Text = "Gooi de dubbelstenen";
+            this.lblP1_displayResults.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblP2_displayResults
+            // 
+            this.lblP2_displayResults.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.lblP2_displayResults.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblP2_displayResults.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblP2_displayResults.Location = new System.Drawing.Point(1030, 565);
+            this.lblP2_displayResults.Name = "lblP2_displayResults";
+            this.lblP2_displayResults.Size = new System.Drawing.Size(304, 64);
+            this.lblP2_displayResults.TabIndex = 13;
+            this.lblP2_displayResults.Text = "Gooi de dubbelstenen";
+            this.lblP2_displayResults.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Rollen2
+            // 
+            this.Rollen2.Location = new System.Drawing.Point(1072, 503);
+            this.Rollen2.Name = "Rollen2";
+            this.Rollen2.Size = new System.Drawing.Size(200, 50);
+            this.Rollen2.TabIndex = 12;
+            this.Rollen2.Text = "Gooi de dobbelstenen";
+            this.Rollen2.UseVisualStyleBackColor = true;
+            this.Rollen2.Click += new System.EventHandler(this.Rollen2_Click);
+            // 
+            // P2dobbel_5
+            // 
+            this.P2dobbel_5.Image = global::Yahtzeespel2.Properties.Resources.blank;
+            this.P2dobbel_5.Location = new System.Drawing.Point(1188, 282);
+            this.P2dobbel_5.Name = "P2dobbel_5";
+            this.P2dobbel_5.Size = new System.Drawing.Size(164, 156);
+            this.P2dobbel_5.TabIndex = 11;
+            // 
+            // P2dobbel_2
+            // 
+            this.P2dobbel_2.Image = global::Yahtzeespel2.Properties.Resources.blank;
+            this.P2dobbel_2.Location = new System.Drawing.Point(1069, 106);
+            this.P2dobbel_2.Name = "P2dobbel_2";
+            this.P2dobbel_2.Size = new System.Drawing.Size(159, 158);
+            this.P2dobbel_2.TabIndex = 10;
+            // 
+            // P2dobbel_3
+            // 
+            this.P2dobbel_3.Image = global::Yahtzeespel2.Properties.Resources.blank;
+            this.P2dobbel_3.Location = new System.Drawing.Point(1257, 112);
+            this.P2dobbel_3.Name = "P2dobbel_3";
+            this.P2dobbel_3.Size = new System.Drawing.Size(155, 152);
+            this.P2dobbel_3.TabIndex = 9;
+            // 
+            // P2dobbel_4
+            // 
+            this.P2dobbel_4.Image = global::Yahtzeespel2.Properties.Resources.blank;
+            this.P2dobbel_4.Location = new System.Drawing.Point(979, 282);
+            this.P2dobbel_4.Name = "P2dobbel_4";
+            this.P2dobbel_4.Size = new System.Drawing.Size(156, 154);
+            this.P2dobbel_4.TabIndex = 8;
+            // 
+            // P2dobbel_1
+            // 
+            this.P2dobbel_1.Image = global::Yahtzeespel2.Properties.Resources.blank;
+            this.P2dobbel_1.Location = new System.Drawing.Point(878, 107);
+            this.P2dobbel_1.Name = "P2dobbel_1";
+            this.P2dobbel_1.Size = new System.Drawing.Size(157, 157);
+            this.P2dobbel_1.TabIndex = 7;
+            // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(604, 22);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(282, 61);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Wachten op rollen";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_p1Name
+            // 
+            this.lbl_p1Name.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_p1Name.Location = new System.Drawing.Point(89, 76);
+            this.lbl_p1Name.Name = "lbl_p1Name";
+            this.lbl_p1Name.Size = new System.Drawing.Size(491, 26);
+            this.lbl_p1Name.TabIndex = 15;
+            // 
+            // lbl_p2Name
+            // 
+            this.lbl_p2Name.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_p2Name.Location = new System.Drawing.Point(876, 76);
+            this.lbl_p2Name.Name = "lbl_p2Name";
+            this.lbl_p2Name.Size = new System.Drawing.Size(491, 30);
+            this.lbl_p2Name.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(1477, 741);
+            this.Controls.Add(this.lbl_p2Name);
+            this.Controls.Add(this.lbl_p1Name);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.lblP2_displayResults);
+            this.Controls.Add(this.Rollen2);
+            this.Controls.Add(this.P2dobbel_5);
+            this.Controls.Add(this.P2dobbel_2);
+            this.Controls.Add(this.P2dobbel_3);
+            this.Controls.Add(this.P2dobbel_4);
+            this.Controls.Add(this.P2dobbel_1);
+            this.Controls.Add(this.lblP1_displayResults);
+            this.Controls.Add(this.Rollen1);
+            this.Controls.Add(this.P1dobbel_5);
+            this.Controls.Add(this.P1dobbel_2);
+            this.Controls.Add(this.P1dobbel_3);
+            this.Controls.Add(this.P1dobbel_4);
+            this.Controls.Add(this.P1dobbel_1);
+            this.MaximumSize = new System.Drawing.Size(5000, 5000);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(800, 600);
+            this.Name = "Form1";
+            this.Text = "Yahtzeespel";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label P1dobbel_1;
+        private System.Windows.Forms.Label P1dobbel_4;
+        private System.Windows.Forms.Label P1dobbel_3;
+        private System.Windows.Forms.Label P1dobbel_2;
+        private System.Windows.Forms.Label P1dobbel_5;
+        private System.Windows.Forms.Button Rollen1;
+        private System.Windows.Forms.Label lblP1_displayResults;
+        private System.Windows.Forms.Label lblP2_displayResults;
+        private System.Windows.Forms.Button Rollen2;
+        private System.Windows.Forms.Label P2dobbel_5;
+        private System.Windows.Forms.Label P2dobbel_2;
+        private System.Windows.Forms.Label P2dobbel_3;
+        private System.Windows.Forms.Label P2dobbel_4;
+        private System.Windows.Forms.Label P2dobbel_1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lbl_p1Name;
+        private System.Windows.Forms.Label lbl_p2Name;
+    }
+}
+
---
+++ Yahtzeespel.cs
@@ -0,0 +1,182 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+#endregion
+
+namespace Yahtzeespel2
+{
+    public partial class Form1 : Form
+    {
+        #region Declaration
+
+        Image[] diceImages;
+        Player player1, player2;
+
+
+        #endregion
+
+        #region Initialization
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            player1 = new Player("Speler1");
+            player2 = new Player("Speler2");
+
+            lbl_p1Name.Text = player1.Name;
+            lbl_p2Name.Text = player2.Name;
+
+            diceImages = new Image[7];
+            diceImages[0] = Properties.Resources.blank;
+            diceImages[1] = Properties.Resources._1;
+            diceImages[2] = Properties.Resources._2;
+            diceImages[3] = Properties.Resources._3;
+            diceImages[4] = Properties.Resources._4;
+            diceImages[5] = Properties.Resources._5;
+            diceImages[6] = Properties.Resources._6;
+
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void Rollen1_Click(object sender, EventArgs e)
+        {
+            if (!player1.Played)
+            {
+                player1.RollDice();
+
+                P1dobbel_1.Image = diceImages[player1.Dice[0]];
+                P1dobbel_2.Image = diceImages[player1.Dice[0]];
+                P1dobbel_3.Image = diceImages[player1.Dice[0]];
+                P1dobbel_4.Image = diceImages[player1.Dice[0]];
+                P1dobbel_5.Image = diceImages[player1.Dice[0]];
+                lblP1_displayResults.Text = player1.Result;
+
+                player1.Played = true;
+
+                CheckWinner();
+
+            }
+        }
+
+        private void Rollen2_Click(object sender, EventArgs e)
+        {
+            if (!player2.Played)
+            {
+                player2.RollDice();
+
+                P2dobbel_1.Image = diceImages[player2.Dice[0]];
+                P2dobbel_2.Image = diceImages[player2.Dice[0]];
+                P2dobbel_3.Image = diceImages[player2.Dice[0]];
+                P2dobbel_4.Image = diceImages[player2.Dice[0]];
+                P2dobbel_5.Image = diceImages[player2.Dice[0]];
+                lblP2_displayResults.Text = player2.Result;
+
+                player2.Played = true;
+
+                CheckWinner();
+
+            }
+        }
+
+        private void CheckWinner()
+        {
+            if (player1.Played && player2.Played)
+            {
+                if (player1.HandRank > player2.HandRank)
+                {
+                    lbl_winnerResult.Text = player1.Name + "Wins!";
+
+                }
+                else if (player2.HandRank > player1.HandRank)
+                {
+                    lbl_winnerResult.Text = player1.Name + "Wins!";
+                }
+                else if (player1.HandRank == 8 && player2.HandRank == 8)
+                {
+                    if (player1.Mod1 > player2.Mod1 &&
+                        player1.Mod1 > player2.Mod2)
+                    {
+                        lbl_winnerResult.Text = player1.Name + "Wins!";
+                    }
+                    else if (player1.Mod2 > player2.Mod1 &&
+                        player1.Mod2 > player2.Mod2)
+                    {
+                        lbl_winnerResult.Text = player1.Name + "Wins!";
+                    }
+                    else if (player1.Mod1 == player2.Mod1 &&
+                        player1.Mod2 == player2.Mod2 ||
+                        player1.Mod2 == player2.Mod1 &&
+                        player1.Mod1 == player2.Mod2)
+                    {
+                        lbl_winnerResult.Text = player1.Name + "Wins!";
+                    }
+                    if (player1.Mod3 > player2.Mod3)
+                    {
+                        lbl_winnerResult.Text = player1.Name + "Wins!";
+                    }
+                    else if (player2.Mod3 > player1.Mod3)
+                    {
+                        lbl_winnerResult.Text = player2.Name + "Wins!";
+                    }
+                    else
+                    {
+                        lbl_winnerResult.Text = player1.Name + " Ties " + player2.Name;
+                    }
+
+
+                }
+            }
+            else if (player1.HandRank == player2.HandRank)
+            {
+                if (player1.Mod1 > player2.Mod1)
+                    lbl_winnerResult.Text = player1.Name + "Wins!";
+                else if (player2.Mod1 > player1.HandRank)
+                    lbl_winnerResult.Text = player2.Name + "Wins!";
+                else if (player1.Mod1 == player2.Mod1)
+                {
+                    if (player1.Mod2 > player2.Mod2)
+                        lbl_winnerResult.Text = player1.Name + "Wins!";
+                    else if (player1.Mod2 > player2.Mod2)
+                        lbl_winnerResult.Text = player2.Name + "Wins!";
+                    else if (player1.Mod2 > player2.Mod2)
+                    {
+                        if (player1.Mod3 > player2.Mod3)
+                            lbl_winnerResult.Text = player1.Name + "Wins!";
+                        else if (player1.Mod3 > player2.Mod3)
+                            lbl_winnerResult.Text = player2.Name + "Wins!";
+                        else if (player1.Mod3 > player2.Mod3)
+                            lbl_winnerResult.Text = player1.Name + " Ties " + player2.Name;
+                    }
+                }
+            }
+            player1.ResetPlayer();
+            player2.ResetPlayer();
+
+        }
+        else if (player1.Played && !player2.Played)
+            lbl_winnerResult.Text = player1.Name + " waiting for " + player2.Name + " to roll: ";
+        else if (player2.Played && !player1.Played)
+            lbl_winnerResult.Text = player1.Name + " waiting for " + player2.Name + " to roll: ";
+     
+
+        #endregion
+
+    }
+}
+
